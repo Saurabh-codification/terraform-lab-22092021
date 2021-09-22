8ee3ae6f4bef7c0f51c482aad48dfc804fc9ddcd
Saurabh <saurabh.dhingra@codification.io>
2021-09-22 13:24:27 +0530

    Added resource Group code

--- resource-group.tf
+++ resource-group.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "resource-group-1" {
-  name     = "myTerraformGroup"
-  location = "West Europe"
+  name     = var.resourceGroupname
+  location = var.location
 
   tags = {
     "env" = "production"

1d7890ca4217413f4a7b7dd092ef702da0c20cf1
Saurabh <saurabh.dhingra@codification.io>
2021-09-22 13:27:00 +0530

    Added resource Group code

--- resource-group.tf
+++ resource-group.tf
@@ -2,7 +2,5 @@ resource "azurerm_resource_group" "resource-group-1" {
   name     = var.resourceGroupname
   location = var.location
 
-  tags = {
-    "env" = "production"
-  }
+  tags = var.tags
 }

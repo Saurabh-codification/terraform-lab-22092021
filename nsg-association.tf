a4d703f461ffeec48a43a778e169bc8ca2dcfe56
Saurabh <saurabh.dhingra@codification.io>
2021-09-22 13:45:18 +0530

    Added resource Group code

--- nsg-association.tf
+++ nsg-association.tf
@@ -1,10 +1,10 @@
-resource "azurerm_subnet_network_security_group_association" "example" {
+resource "azurerm_subnet_network_security_group_association" "nsg-association1" {
   subnet_id                 = azurerm_subnet.subnet1.id
   
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-resource "azurerm_subnet_network_security_group_association" "example" {
+resource "azurerm_subnet_network_security_group_association" "nsg-association2" {
   subnet_id                 = azurerm_subnet.subnet2.id
   
   network_security_group_id = azurerm_network_security_group.nsg.id
